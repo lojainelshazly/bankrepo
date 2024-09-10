3036b444d84fbbdc8c7a69988294cb0162b81b58
شهد رشاد ابراهيم الأحول <ug_31111240@art.tanta.edu.eg>
2024-09-10 19:44:30 +0300

    Update File_Manager.h

--- my pro/File_Manager.h
+++ my pro/File_Manager.h
@@ -1,6 +1,61 @@
-#pragma once
-class File_Manager
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include"Client.h"
+#include"Admin.h"
+#include"Employee.h"
+#include"Datasourceinterface.h"
+using namespace std;
+class File_Manager :public Datasourceinterface
 {
+	
+	string file1 = "client.txt";
+	string file2 = "employee.txt";
+	string file3 = "admins.txt";
+	
+
+	
+
+	void addclient(Client& client) {
+		Client c;
+		fstream file(file1, ios::app);
+		file << c.getName()<<c.getId()<<c.getPassword()<<endl;
+		file.close();
+	}
+	Employee e;
+	void addemployee(Employee& employee) {
+		fstream file(file2, ios::app);
+		file <<e.getName()<<e.getId()<<e.getPassword()<<endl ;
+		file.close();
+	}
+	Admin a;
+	void addadmin(Admin& admin) {
+		fstream file(file3, ios::app);
+		file << a.getName() << a.getId() << a.getPassword() << endl; ;
+		file.close();
+	}
+//=========
+	vector<Client>getAllClients;
+	vector<Employee>getAllEmployees;
+	vector<Admin>getAllAdmins;
+//==================
+	void removeallclients() {
+		ofstream file1(file1, ios::trunc);
+
+	}
+
+	void removeallemployee() {
+		ofstream file2(file2, ios::trunc);
+
+	}
+	void removealladmins() {
+		ofstream file1(file3, ios::trunc);
+		
+	}
+
+
+
+
 
 };
-
